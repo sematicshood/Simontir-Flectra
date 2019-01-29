a2eff628c4908bb0e0c65709124de4463203cb18
nopezi saputra <snopezi@gmail.com>
2019-01-29 14:45:10 +0700

    module project

---
+++ addons/project/tests/test_project_ui.py
@@ -0,0 +1,11 @@
+# Part of Odoo, Flectra. See LICENSE file for full copyright and licensing details.
+
+import flectra.tests
+
+
+@flectra.tests.common.at_install(False)
+@flectra.tests.common.post_install(True)
+class TestUi(flectra.tests.HttpCase):
+
+    def test_01_project_tour(self):
+        self.phantom_js("/web", "flectra.__DEBUG__.services['web_tour.tour'].run('project_tour')", "flectra.__DEBUG__.services['web_tour.tour'].tours.project_tour.ready", login="admin")
