7dc786f9530eb251789dcc8e7c09dceb38d21373
Rosyid Ridho <rosyidnova@gmail.com>
2019-02-07 13:19:17 +0700

    update

--- addons/management_api/models/sale.py
+++ addons/management_api/models/sale.py
@@ -6,8 +6,8 @@ class roles_users(models.Model):
     _inherit = 'sale.order'
 
     x_estimasi_waktu    =   fields.Char()
-    x_nomer_polisi      =   fields.Char()
-    x_tipe_kendaraan    =   fields.Char()
+    x_nopol             =   fields.Char()
+    x_type_motor        =   fields.Char()
     x_antrian_service   =   fields.Char()
     x_waktu_mulai       =   fields.Datetime()
     x_is_reject         =   fields.Boolean(default=False)
