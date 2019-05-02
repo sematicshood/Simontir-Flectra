f9dc2671616b474e35ee24ca015de62ac79d0675
Bima Pangestu <bimapangestu280@gmail.com>
2019-05-03 05:41:19 +0700

    fix bug

--- addons/management_api/controllers/products.py
+++ addons/management_api/controllers/products.py
@@ -6,18 +6,20 @@ from . rest_exception import *
 import datetime
 import traceback
 
+
 class ProductsAPIBentar(http.Controller):
     @http.route('/simontir/products/search', type='http', auth='none', methods=['GET', 'OPTIONS'], csrf=False, cors="*")
     # @authentication
-    def productSearch(self, barcode = None, name = None, description = None, type = None, vehicle = None, page = 0, register = None):
+    def productSearch(self, barcode=None, name=None, description=None, type=None, vehicle=None, page=0, register=None):
         try:
-            res    = []
+            res = []
 
             search = []
 
-            fields = ['name', 'barcode', 'qty_available', 'list_price', 'type', 'sales_count', 'minimal_km']
+            fields = ['name', 'barcode', 'qty_available',
+                      'list_price', 'type', 'sales_count', 'minimal_km']
 
-            limit  = 10
+            limit = 10
             offset = int(page)
             offset = ((offset - 1) if offset > 0 else 0) * limit
 
@@ -38,28 +40,38 @@ class ProductsAPIBentar(http.Controller):
                     search.append(('registrasi', '=', True))
 
             if vehicle != None:
-                products = request.env['fleet.vehicle.model'].sudo().search_read([('id','=',vehicle)], fields=['x_product_ids'])[0]['x_product_ids']
+                products = request.env['fleet.vehicle.model'].sudo().search_read(
+                    [('id', '=', vehicle)], fields=['x_product_ids'])[0]['x_product_ids']
 
-                search.append(('product_tmpl_id','in',products))
+                search.append(('product_tmpl_id', 'in', products))
 
-            res = request.env['product.product'].sudo().search_read(search, fields=fields, limit=limit, offset=offset, order="sales_count desc")
+            res = request.env['product.product'].sudo().search_read(
+                search, fields=fields, limit=limit, offset=offset, order="sales_count desc")
             total = request.env['product.product'].sudo().search_count(search)
 
+            if total <= 0:
+                del search[-1]
+                res = request.env['product.product'].sudo().search_read(
+                    search, fields=fields, limit=limit, offset=offset, order="sales_count desc")
+                total = request.env['product.product'].sudo(
+                ).search_count(search)
+
             return valid_response(status=200, data={
-                    'count': total,
-                    'results': res
-                })
+                'count': total,
+                'results': res
+            })
         except Exception as identifier:
             print(traceback.format_exc())
 
     @http.route('/simontir/nopol/search', type='http', auth='none', methods=['GET', 'OPTIONS'], csrf=False, cors="*")
     # @authentication
-    def nopolSearch(self, nopol = None):
+    def nopolSearch(self, nopol=None):
         res = []
 
-        res      = request.env['fleet.vehicle'].sudo().search_read([('license_plate', 'ilike', nopol)], fields=['license_plate'], limit=10)
+        res = request.env['fleet.vehicle'].sudo().search_read(
+            [('license_plate', 'ilike', nopol)], fields=['license_plate'], limit=10)
 
         return valid_response(status=200, data={
-                'count': len(res),
-                'results': res
-            })
+            'count': len(res),
+            'results': res
+        })
