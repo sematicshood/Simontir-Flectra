91e749de621c83f73efd5c87758a9b4d67edaf0b
Bima Pangestu <bimapangestu280@gmail.com>
2019-05-07 10:58:36 +0700

    printer

---
+++ addons/report-print-send-11.0/setup/_metapackage/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo11-addons-oca-report-print-send",
+    description="Meta package for oca-report-print-send Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo11-addon-base_report_to_printer',
+        'odoo11-addon-printer_zpl2',
+        'odoo11-addon-remote_report_to_printer',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+    ]
+)
