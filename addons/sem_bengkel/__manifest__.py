06b0a2a269004b9ca97fea1798d1555d7786f485
dennyard <sematics.info@gmail.com>
2019-07-20 16:37:40 +0700

    Update sem_bengkel
    update sembengkel modul.
    Add nopol

--- addons/sem_bengkel/__manifest__.py
+++ addons/sem_bengkel/__manifest__.py
@@ -7,26 +7,27 @@
         subtitle on modules listing or apps.openerp.com""",
 
     'description': """
-        Long description of module's purpose
+        Penambahan modul utk bisnis service khususnya bengkel
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Sematics",
+    'website': "http://sematics.id",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/flectra/flectra/blob/master/flectra/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Development',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','sale'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
+        # 'views/templates.xml',
+        'views/custom_field_saleorder.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
