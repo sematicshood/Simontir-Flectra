a6303d30d319c2b140ea58bd574417c9d5bc5d01
dennyard <sematics.info@gmail.com>
2019-08-18 17:43:04 +0700

    update print out invoice
    update print out invoice

--- addons/print_dotmatrix/__manifest__.py
+++ addons/print_dotmatrix/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale', 'account', 'mail', 'web'],
+    'depends': ['base', 'sale', 'account', 'mail', 'web','fleet'],
 
     # always loaded
     'data': [
