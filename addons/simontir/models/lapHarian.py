b1e7b6803f8722142a53b2c26acf536e8a873b76
dennyard <sematics.info@gmail.com>
2019-09-28 13:34:49 +0700

    update simontir lap harian
    update simontir lap harian

--- addons/simontir/models/lapHarian.py
+++ addons/simontir/models/lapHarian.py
@@ -22,6 +22,10 @@ class LapHarian(models.Model):
     totalAss2 = fields.Integer()
     totalAss3 = fields.Integer()
     totalAss4 = fields.Integer()
+    totalGo = fields.Integer()
+    totalHr = fields.Integer()
+    totalQs = fields.Integer()
+    totalGop = fields.Integer()
     selisih = fields.Float()
     totalCash = fields.Float()
     totalEdc = fields.Float()
@@ -54,43 +58,61 @@ class LapHarian(models.Model):
         x_omsetjasa = 0
         x_omsetglobal = 0
         x_omsetpart = 0
+        x_omsetpartoli = 0
+        x_omsetpartbusi = 0
+        x_omsetkpboli = 0
+        x_invblmlunas = 0
         ass_1 = 0
         ass_2 = 0
         ass_3 = 0
         ass_4 = 0
         for sol in so:
-             if sol['x_kpb'] == '1':
+              if sol['x_kpb'] == '1':
                  ass_1 += 1
                  if sol['x_kpb'] == '2':
-                     ass_2 += 1
+                    ass_2 += 1
                  if sol['x_kpb'] == '3':
-                     ass_3 += 1
-             else:
-                 ass_4 += 1
+                    ass_3 += 1
+                 if sol['x_kpb'] == '4':
+                    ass_4 += 1
 
         # x_omsetglobal += sol['amount_total']
             # print(x_omset)
             # print(sol['name'])
-
+#TODO validasi status invoice lunas
+#TODO validasi KPB khusus
         for invl in inv:
             # print(invl.name)
             nosonya = invl.origin
             sonya = self.env['sale.order'].sudo().search([
                 ('name', '=', nosonya)])
-
+            issokpb = sonya.x_kpb
+            fakturnya = self.env['account.invoice'].sudo().search([
+                ('id', '=', invl.invoice_id)])
+            statusinv = fakturnya.state
             id_product = invl.product_id
             typeprod = id_product.type
-            if typeprod == 'service':
-                x_omsetjasa += invl['price_total']
+            categprod = id_product.categ_id
+            if statusinv == 'paid':  # validasi status invoice yg sudah dibayar
+                if typeprod == 'service':  # validasi tipe produk yg jasa / service
+                    x_omsetjasa += invl['price_total']
+                else:  # validasi tipe produk yg stockable/sparepart
+                    x_omsetpart += invl['price_total']
+                    if categprod == 'Oli':  # validasi tipe part yg kategori oli
+                        x_omsetpartoli += invl['price_total']
+                        if issokpb > 0: # validasi tipe produk yg jasa / service utk oli khusus kpb
+                           x_omsetkpboli += invl['price_total']
+                    if categprod == 'Busi': # validasi tipe part yg kategori busi
+                        x_omsetpartbusi += invl['price_total']
             else:
-                x_omsetpart += invl['price_total']
+                x_invblmlunas += invl['price_total']
 
         self.omsetJasa = x_omsetjasa
         self.omsetPart = x_omsetpart
-        self.omsetKpbOli = float(100000)
+        self.omsetKpbOli = x_omsetkpboli
         self.biayaHarian = float(12000)
-        self.omsetPartBusi = float(120)
-        self.omsetPartOli = float(29000)
+        self.omsetPartBusi = x_omsetpartbusi
+        self.omsetPartOli = x_omsetpartoli
         self.omsetPos = float(1000000)
         self.omsetTotalJasa = float(self.omsetJasa) + float(self.omsetKpbOli)
         self.omsetTotalPart = self.omsetPartBusi + self.omsetPartOli + self.omsetPart
